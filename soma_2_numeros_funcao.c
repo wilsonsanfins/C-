9a96d8c2a752ac35c31c6023152cb291e3ba00ef
wilsonsanfins <162934920+wilsonsanfins@users.noreply.github.com>
2024-10-06 17:50:46 -0300

    Add files via upload

---
+++ soma_2_numeros_funcao.c
@@ -0,0 +1,16 @@
+main()
+{
+    int x , y , r;
+    printf("entre com 2 numeros: " );
+    scanf("%d %d ",&x,&y);
+    r=soma(x,y);
+    printf("a soma é : %d " ,r)
+}
+
+/*soma()*/
+/*retorna a soma dos 2 numeros*/
+soma(j,k)
+int j , k;
+{
+    return(j+k);
+}
